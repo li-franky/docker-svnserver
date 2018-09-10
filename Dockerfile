aa198ad539baf9565c8f1abeb94d7580bd28bd45
li-franky <li-franky@163.com>
2018-09-10 22:35:35 +0800

    添加构建docker镜像的文件

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+FROM centos:7.4.1708
+MAINTAINER FrankyLee li-franky@163.com
+LABEL name="SVN Server with Apache - Image Base CentOS" \
+    vendor="FrankyLee <li-franky@163.com>" \
+    license="apache license 2.0" \
+    build-date="20180910"
+
+COPY wandisco-svn.repo /etc/yum.repos.d/wandisco-svn.repo
+COPY initfile /opt/initfile
+
+RUN yum install httpd php -y && \
+    curl https://codeload.github.com/mfreiholz/iF.SVNAdmin/tar.gz/stable-1.6.2 >> svnadmin.tar.gz && \
+    tar zxvf svnadmin.tar.gz && \
+    mv iF.SVNAdmin-stable-1.6.2/* /var/www/html && \
+    rm -rf svnadmin.tar.gz iF.SVNAdmin-stable-1.6.2 && \
+    yum --disablerepo="*" --enablerepo="WandiscoSVN" install subversion mod_dav_svn -y
+
+COPY subversion.conf /etc/httpd/conf.d/subversion.conf
+COPY initfile/init.service /etc/init.d/init.service
+
+RUN chmod 700 /etc/init.d/init.service && \
+    chkconfig --add init.service
+
+# web管理端口
+EXPOSE 80
+
+HEALTHCHECK CMD netstat -ln | grep 80 || exit 1
+VOLUME [ "/var/www/html/data" ]
+WORKDIR /var/www/html/data
+
+CMD ["/usr/sbin/init"]
---
+++ initfile/init.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+/usr/sbin/init
+chown -R apache:apache /var/www/html/data
+#chmod -R 777 /var/www/html/data
+
+filepath="/var/www/html/data/config.tpl.ini"
+if [ ! -x "$filepath" ]; then
+  cp /opt/initfile/config.tpl.ini /var/www/html/data/config.tpl.ini
+  chown apache:apache /var/www/html/data/config.tpl.ini
+fi
+
+filepath="/var/www/html/data/authz"
+if [ ! -x "$filepath" ]; then
+  touch /var/www/html/data/authz
+  chown apache:apache /var/www/html/data/authz
+fi
+
+filepath="/var/www/html/data/passwd"
+if [ ! -x "$filepath" ]; then
+  touch /var/www/html/data/passwd
+  chown apache:apache /var/www/html/data/passwd
+fi
+
+filepath="/var/www/html/data/config.ini"
+if [ ! -x "$filepath" ]; then
+  cp /opt/initfile/config.ini /var/www/html/data/config.ini
+  chown apache:apache /var/www/html/data/config.ini
+fi
+
+dirpath="/var/www/html/data/backup"
+if [ ! -x "$dirpath" ]; then
+  mkdir /var/www/html/data/backup
+  chown -R apache:apache /var/www/html/data/backup
+fi
+dirpath="/var/www/html/data/repositories"
+if [ ! -x "$dirpath" ]; then
+  mkdir /var/www/html/data/repositories
+  chown -R apache:apache /var/www/html/data/repositories
+fi
+
+
+systemctl restart httpd
+
+tail -f /dev/null
+
